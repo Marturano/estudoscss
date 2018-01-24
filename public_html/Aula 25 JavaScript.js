53f6e0f33bd1af92be02b1e5e6cfdc8a1077f48e
Unknown <samuelbarbelli@hotmail.com>
2018-01-24 12:18:19 -0400

    2018.01.24 - 21.18 - samuel

--- public_html/Aula 25 JavaScript.js
+++ public_html/Aula 25 JavaScript.js
@@ -1,4 +1,8 @@
 
-function mudaFoto(foto) {
-    document.getElementById("icone").src = foto;
+function calc_Total() {
+    
+    var Amount = parseInt(document.getElementById('cAmount').value);
+    Price = Amount * 1500;
+    document.getElementById('cPrice').value = Price;
 }
+
